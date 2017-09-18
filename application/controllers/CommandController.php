bcd55b76b1d8dcd2c72da4b5b207646506fc314c
cstegm <cstegm@users.noreply.github.com>
2017-09-18 10:24:04 +0200

    Update CommandController.php

--- application/controllers/CommandController.php
+++ application/controllers/CommandController.php
@@ -5,38 +5,43 @@ namespace Icinga\Module\Actions\Controllers;
 
 use Icinga\Web\Controller;
 use Icinga\Web\Navigation\Navigation;
-use Icinga\Application\Config;	
+use Icinga\Application\Config;
+use Icinga\Authentication\Auth;
 
 class CommandController  extends Controller
 {
     public function indexAction()
-    {        
-	    $auth = Auth::getInstance();
-            if ($auth->hasPermission('actions/command')){
+    {
+        $auth = Auth::getInstance();
 
-		    $host = $this->params->getRequired('host');
+        if ($auth->hasPermission('actions/command')){
+            $host = $this->params->getRequired('host');
+            $this->view->auth=1;
 
-		    $action = $this->params->getRequired('action');
-		    $config = Config::app('modules/actions/actions');
-		    $command = str_replace('$host_name$',$host,$config->get($action,"command"));
-		    $this->view->command = $command;
-		    $this->view->action = $action;
-		    $this->view->output .= shell_exec($command." 2>&1");
-		    $this->getTabs();
-	    }
+            $action = $this->params->getRequired('action');
+            $config = Config::app('modules/actions/actions');
+            $command = str_replace('$host_name$',$host,$config->get($action,"command"));
+            $this->view->command = $command;
+            $this->view->action = $action;
+            $this->view->output .= shell_exec($command." 2>&1");
+            $this->getTabs();
+        }else{
+            $this->view->auth=0;
+            $this->view->authmsg=$this->translate("You do not have the permission to execute actions.");
+        }
+
     }
     public function getTabs()
-	{
-		$tabs = parent::getTabs();
-		$tabs->add(
-			'action',
-			array(
-				'title' => 'Action',
-				'url' => 'actions/config/update?action='.$this->view->action,
-				'baseTarget' => '_main'
-			)
-		);
-		return $tabs;
-	} 
+        {
+                $tabs = parent::getTabs();
+                $tabs->add(
+                        'action',
+                        array(
+                                'title' => 'Action',
+                                'url' => 'actions/config/update?action='.$this->view->action,
+                                'baseTarget' => '_main'
+                        )
+                );
+                return $tabs;
+        }
 }
-
