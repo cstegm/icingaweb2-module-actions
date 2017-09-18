ae55b98a11c9891bef68c8053ed504d0a78de64a
cstegm <cstegm@users.noreply.github.com>
2017-09-18 10:07:13 +0200

    Update CommandController.php

--- application/controllers/CommandController.php
+++ application/controllers/CommandController.php
@@ -10,16 +10,20 @@ use Icinga\Application\Config;
 class CommandController  extends Controller
 {
     public function indexAction()
-    {
-	    $host = $this->params->getRequired('host');
-	    
-	    $action = $this->params->getRequired('action');
-            $config = Config::app('modules/actions/actions');
-	    $command = str_replace('$host_name$',$host,$config->get($action,"command"));
-	    $this->view->command = $command;
-	    $this->view->action = $action;
-	    $this->view->output .= shell_exec($command." 2>&1");
-	    $this->getTabs();
+    {        
+	    $auth = Auth::getInstance();
+            if ($auth->hasPermission('actions/command')){
+
+		    $host = $this->params->getRequired('host');
+
+		    $action = $this->params->getRequired('action');
+		    $config = Config::app('modules/actions/actions');
+		    $command = str_replace('$host_name$',$host,$config->get($action,"command"));
+		    $this->view->command = $command;
+		    $this->view->action = $action;
+		    $this->view->output .= shell_exec($command." 2>&1");
+		    $this->getTabs();
+	    }
     }
     public function getTabs()
 	{
